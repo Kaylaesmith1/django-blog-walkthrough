dbc205915b49b33f06a4fffa6f15c8f158f4da33
Kayla S <kaylaesmith1@gmail.com>
2023-03-28 16:31:59 +0000

    Comment success pop up message. Final commit.

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
-
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -76,4 +75,5 @@ class PostLike(View):
             post.likes.add(request.user)
         
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-        
+
+
